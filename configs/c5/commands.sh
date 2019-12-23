02c0787ae152d10d88dc80e24c8fbfc1a370d1f5
mistrate <mistrate@luxoft.com>
2019-12-23 03:22:57 -0800

    c5 yamls

--- configs/c5/commands.sh
+++ configs/c5/commands.sh
@@ -4,5 +4,10 @@ kubectl run kuard --generator=run-pod/v1 \--image=hub.docker.com/r/selenium/hub
 kubectl get pods
 #Delete pod
 kubectl delete pods/kuard
+kubectl delete -f kuard.yaml
 #Deploy selenium yaml pod
-kubectl apply -f web_server.yaml
+kubectl apply -f kuard.yaml
+#Get pod details <kubectl describe pods <pod_name>>
+kubectl describe pods kuard
+#Port forwarding
+kubectl port-forward kuard 8080:8080
